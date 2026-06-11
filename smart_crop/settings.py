195e81d973a29b468319c72e7f7ac81bdb4b9970
Md Obaydullah Al Numan <numan.obaydullah@gmail.com>
2026-06-11 15:38:39 +0600

    Move authentication template overrides to global templates directory and update settings DIRS

--- smart_crop/settings.py
+++ smart_crop/settings.py
@@ -132,7 +132,7 @@ ROOT_URLCONF = 'smart_crop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
